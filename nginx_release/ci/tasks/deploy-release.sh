e7ab76bb1fbdbff5a6fda259cf2f4ee09b1eafa7
Russ Baker <russ.baker@digitalglobe.com>
2018-11-08 21:46:42 +0000

    Needed to add bosh deployment.

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export CA_CERT_URL=https://unreal-snw.s3.amazonaws.com/training-bosh.pem
-export BOSH_DEPLOYMENT=<replace-me>-nginx
+export BOSH_DEPLOYMENT=rbaker>-nginx
 export BOSH_DIRECTOR=https://10.200.192.0:25555/
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
