fe3c724c998a4c7ea34669c13ae5d8ec80e20fc6
Russ Baker <russ.baker@digitalglobe.com>
2018-11-08 21:56:24 +0000

    Getting correct ip for the bosh director.

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -2,7 +2,7 @@
 
 export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/training-bosh.pem
 export BOSH_DEPLOYMENT=rbaker-nginx
-export BOSH_DIRECTOR=https://10.200.192.0:25555/
+export BOSH_DIRECTOR=https://10.4.1.4:25555/
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_ENVIRONMENT=training-bosh
