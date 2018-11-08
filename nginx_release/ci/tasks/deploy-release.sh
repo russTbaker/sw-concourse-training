ec5ab58860b2271c91a40fe46f08211e7d5f5eb6
Russ Baker <russ.baker@digitalglobe.com>
2018-11-08 21:57:41 +0000

    Use correct deployment.

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export CA_CERT_URL=https://s3.us-east-2.amazonaws.com/dg-training-concourse/training-bosh.pem
-export BOSH_DEPLOYMENT=rbaker-nginx
+export BOSH_DEPLOYMENT=russTbaker-nginx
 export BOSH_DIRECTOR=https://10.4.1.4:25555/
 export BOSH_CLIENT=admin
 export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
