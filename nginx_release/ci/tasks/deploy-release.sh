b09f19ec807af7146836bbc5cd5fe69ecd1b3e70
Russ Baker <russ.baker@digitalglobe.com>
2018-11-08 21:44:45 +0000

    Missed a secret... sshhhhhh

--- nginx_release/ci/tasks/deploy-release.sh
+++ nginx_release/ci/tasks/deploy-release.sh
@@ -4,7 +4,7 @@ export CA_CERT_URL=https://unreal-snw.s3.amazonaws.com/training-bosh.pem
 export BOSH_DEPLOYMENT=<replace-me>-nginx
 export BOSH_DIRECTOR=https://10.200.192.0:25555/
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=<replace-me>
+export BOSH_CLIENT_SECRET=kDuaJ8nD0Ky5GGLXntsFeg88M934a3
 export BOSH_ENVIRONMENT=training-bosh
 
 cd source-code/nginx_release
